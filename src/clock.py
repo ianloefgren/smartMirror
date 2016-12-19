bd5f33ea13085c4584161c187fa987ad832e00d3
Ian Loefgren <ian.loefgren@colorado.edu>
2016-12-19 01:09:48 -0700

    implemented clock class with date and time displaying capability

--- src/clock.py
+++ src/clock.py
@@ -1,3 +1,43 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
-class Clock
+from Tkinter import *
+import time
+
+class Clock(Frame):
+	def __init__(self,parent,*args,**kwargs):
+		Frame.__init__(self,parent,bg='black')
+		# create time label
+		self.time1 = ''
+		self.timeLabel = Label(self,font=('Helvetica',18),fg='white',bg='black')
+		self.timeLabel.pack(side=TOP,anchor=E)
+		
+		# create day of week label
+		self.dow1 = ''
+		self.dowLabel = Label(self,font=('Helvetica',12), fg='white',bg='black')
+		self.dowLabel.pack(side=TOP,anchor=E)
+		
+		# create date label
+		self.date1 = ''
+		self.dateLabel = Label(self,font=('Helvetica',12), fg='white',bg='black')
+		self.dateLabel.pack(side=TOP,anchor=E)
+		
+		self.update()
+		
+
+	def update(self):
+		time2 = time.strftime('%I:%M %p')
+		dow2 = time.strftime('%A')
+		date2 = time.strftime("%b %d, %Y")
+
+		if time2 != self.time1:
+			self.time1 = time2
+			self.timeLabel.config(text=time2)
+		if dow2 != self.dow1:
+			self.dow1 = dow2
+			self.dowLabel.config(text=dow2)
+		if date2 != self.date1:
+			self.date1 = date2
+			self.dateLabel.config(text=date2)
+
+		self.timeLabel.after(200, self.update)
+
---
+++ src/main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+from Tkinter import *
+import locale
+import threading
+import time
+
+from clock import Clock
+
+class FullscreenWindow:
+	def __init__(self):
+		self.tk = Tk()
+		self.tk.configure(background='black')
+		self.topFrame = Frame(self.tk,background='black')
+		self.bottomFrame = Frame(self.tk,background='black')
+		self.topFrame.pack(side=TOP,fill=BOTH,expand=YES)
+		self.bottomFrame.pack(side=TOP,fill=BOTH,expand=YES)
+		self.state = True
+		self.tk.bind("<Return>",self.toggle_fullscreen)
+		self.tk.bind("<Enter>",self.end_fullscreen)
+
+		# clock
+		self.clock = Clock(self.topFrame)
+		self.clock.pack(side=RIGHT,anchor=N,padx=100,pady=60)		
+
+
+	def toggle_fullscreen(self, event=None):
+		self.state = not self.state #toggle boolean
+		self.tk.attributes("-fullscreen",self.state)
+		return "break"
+
+	def end_fullscreen(self, event=None):
+		self.state = False
+		self.tk.attributes("-fullscreen",self.state)
+		return "break"
+
+if __name__ == '__main__':
+	w = FullscreenWindow()
+	w.tk.mainloop()
+	
